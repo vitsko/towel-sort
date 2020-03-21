73b65f2d57bab1c3917d4cc5a1fefff71a2e6cca
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-21 19:19:18 +0400

    feat: add solution task 'Towel Sort'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let matrixAfterSorting = [];
+
+  if (matrix === undefined) {
+    return matrixAfterSorting;
+  }
+
+  for (let i = 0; i < matrix.length; ++i) {
+    for (let j = 0; j < matrix[i].length; ++j) {
+      matrixAfterSorting.push(i % 2 === 0
+        ? matrix[i][j]
+        : matrix[i][matrix[i].length - j - 1]);
+    }
+  }
+
+  return matrixAfterSorting;
 }
